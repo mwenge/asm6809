70c4229139b315e9d80670bceaa4ee254357fcf3
Ciaran Anscomb <xroar@6809.org.uk>
2014-03-24 06:53:24 +0000

    add array.h

---
+++ src/array.h
@@ -0,0 +1,13 @@
+/*
+ * asm6809 - a 6809 assembler
+ * Copyright 2013-2014 Ciaran Anscomb
+ *
+ * See COPYING.GPL for redistribution conditions.
+ */
+
+#ifndef ASM6809_ARRAY_H_
+#define ASM6809_ARRAY_H_
+
+#define ARRAY_N_ELEMENTS(a) (sizeof(a) / sizeof((a)[0]))
+
+#endif  /* ASM6809_ARRAY_H_ */
