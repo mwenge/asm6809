9f4252afc31be6e3a5a38934671127955aa57cfe
Ciaran Anscomb <xroar@6809.org.uk>
2014-04-24 21:55:31 +0100

    update version text

--- src/asm6809.c
+++ src/asm6809.c
@@ -251,7 +251,13 @@ static void helptext(void) {
 }
 
 static void versiontext(void) {
-	puts("asm6809 " PACKAGE_VERSION);
+	puts(
+"asm6809 " PACKAGE_VERSION "\n"
+"Copyright (C) 2014 Ciaran Anscomb\n"
+"License: GNU GPL version 3 or later <http://www.gnu.org/licenses/gpl-3.0.html>.\n"
+"This is free software: you are free to change and redistribute it.\n"
+"There is NO WARRANTY, to the extent permitted by law."
+	);
 }
 
 /* Call the various free_all routines to tidy up memory.  Allows us to see
