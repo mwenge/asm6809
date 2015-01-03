89b20c5edcf477a6ee9844a54bb114307a051beb
Ciaran Anscomb <xroar@6809.org.uk>
2015-01-03 23:02:04 +0000

    speed up local label searching
    Use a binary tree for storing line numbers of duplicate local labels.

--- src/symbol.c
+++ src/symbol.c
@@ -17,15 +17,16 @@ option) any later version.
 #include <stdlib.h>
 #include <string.h>
 
+#include "dict.h"
+#include "gl_avltree_list.h"
+#include "gl_xlist.h"
 #include "xalloc.h"
 
 #include "assemble.h"
-#include "dict.h"
 #include "error.h"
 #include "eval.h"
 #include "node.h"
 #include "section.h"
-#include "slist.h"
 #include "symbol.h"
 
 /*
@@ -120,27 +121,67 @@ static void symbol_local_free(struct symbol_local *sym) {
 	free(sym);
 }
 
-static void symbol_local_list_free(struct slist *sym_list) {
-	slist_free_full(sym_list, (slist_free_func)symbol_local_free);
+static void symbol_local_list_free(gl_list_t sym_list) {
+	gl_list_iterator_t iter = gl_list_iterator(sym_list);
+	const void *elt;
+	gl_list_node_t node;
+	while (gl_list_iterator_next(&iter, &elt, &node)) {
+		gl_list_remove_node(sym_list, node);
+	}
+	gl_list_iterator_free(&iter);
+	gl_list_free(sym_list);
 }
 
 struct dict *symbol_local_table_new(void) {
-	return dict_new_full(dict_direct_hash, dict_direct_equal, NULL, (slist_free_func)symbol_local_list_free);
+	return dict_new_full(dict_direct_hash, dict_direct_equal, NULL, (Hash_data_freer)symbol_local_list_free);
+}
+
+static struct symbol_local const *sym_found = NULL;
+
+static bool sym_local_equals(const void *a, const void *b) {
+	struct symbol_local const *syma = a, *symb = b;
+	return syma->line_number == symb->line_number;
+}
+
+static int sym_local_compar(const void *a, const void *b) {
+	struct symbol_local const *syma = a, *symb = b;
+	if (syma->line_number < symb->line_number)
+		return -1;
+	if (syma->line_number > symb->line_number)
+		return 1;
+	return 0;
+}
+
+static int sym_local_compar_backref(const void *a, const void *b) {
+	struct symbol_local const *syma = a, *symb = b;
+	if (syma->line_number > symb->line_number)
+		return 1;
+	if (!sym_found || syma->line_number > sym_found->line_number)
+		sym_found = syma;
+	if (syma->line_number < symb->line_number)
+		return -1;
+	return 0;
+}
+
+static int sym_local_compar_fwdref(const void *a, const void *b) {
+	struct symbol_local const *syma = a, *symb = b;
+	if (syma->line_number <= symb->line_number)
+		return -1;
+	if (!sym_found || syma->line_number < sym_found->line_number)
+		sym_found = syma;
+	if (syma->line_number > symb->line_number+1)
+		return 1;
+	return 0;
 }
 
 struct node *symbol_local_backref(struct dict *table, long key, unsigned line_number) {
-	struct slist *sym_list = dict_lookup(table, (void *)key);
+	gl_list_t sym_list = dict_lookup(table, (void *)key);
 	if (sym_list) {
-		struct symbol_local *sym = NULL;
-		for (struct slist *l = sym_list; l; l = l->next) {
-			struct symbol_local *ls = l->data;
-			if (ls->line_number <= line_number &&
-			    (!sym || ls->line_number > sym->line_number)) {
-				sym = ls;
-			}
-		}
-		if (sym) {
-			return node_ref(sym->node);
+		sym_found = NULL;
+		struct symbol_local find = { .line_number = line_number };
+		(void)gl_sortedlist_search(sym_list, sym_local_compar_backref, &find);
+		if (sym_found) {
+			return node_ref(sym_found->node);
 		}
 	}
 	error(error_type_inconsistent, "backref '%ld' not defined", key);
@@ -148,48 +189,44 @@ struct node *symbol_local_backref(struct dict *table, long key, unsigned line_nu
 }
 
 struct node *symbol_local_fwdref(struct dict *table, long key, unsigned line_number) {
-	struct slist *sym_list = dict_lookup(table, (void *)key);
+	gl_list_t sym_list = dict_lookup(table, (void *)key);
 	if (sym_list) {
-		struct symbol_local *sym = NULL;
-		for (struct slist *l = sym_list; l; l = l->next) {
-			struct symbol_local *ls = l->data;
-			if (ls->line_number > line_number &&
-			    (!sym || ls->line_number < sym->line_number)) {
-				sym = ls;
-			}
-		}
-		if (sym) {
-			return node_ref(sym->node);
+		sym_found = NULL;
+		struct symbol_local find = { .line_number = line_number };
+		(void)gl_sortedlist_search(sym_list, sym_local_compar_fwdref, &find);
+		if (sym_found) {
+			return node_ref(sym_found->node);
 		}
 	}
-	error(error_type_inconsistent, "fwdref '%ld' not defined", key);
+	error(error_type_inconsistent, "backref '%ld' not defined", key);
 	return NULL;
 }
 
 void symbol_local_set(struct dict *table, long key, unsigned line_number, struct node *value,
 		      unsigned pass) {
-	struct slist *sym_list = dict_lookup(table, (void *)key);
-	dict_steal(table, (void *)key);
-	struct symbol_local *sym = NULL;
-	for (struct slist *l = sym_list; l; l = l->next) {
-		struct symbol_local *ls = l->data;
-		if (ls->line_number == line_number) {
-			sym = ls;
-			break;
-		}
+	gl_list_t sym_list = dict_lookup(table, (void *)key);
+	gl_list_node_t old_lnode = NULL;
+	struct symbol_local const *old_sym = NULL;
+	if (!sym_list) {
+		sym_list = gl_list_create_empty(GL_AVLTREE_LIST, sym_local_equals, NULL, (gl_listelement_dispose_fn)symbol_local_free, 0);
+		dict_insert(table, (void *)key, sym_list);
+	} else {
+		struct symbol_local find = { .line_number = line_number };
+		old_lnode = gl_list_search(sym_list, &find);
+		if (old_lnode)
+			old_sym = gl_list_node_value(sym_list, old_lnode);
 	}
 	struct node *newn = eval_node(value);
-	if (sym) {
-		if (!node_equal(sym->node, newn))
+	struct symbol_local *new_sym = xmalloc(sizeof(*new_sym));
+	new_sym->line_number = line_number;
+	new_sym->node = newn;
+	if (old_sym) {
+		if (!node_equal(old_sym->node, newn))
 			error(error_type_inconsistent,
 			      "value of local label '%ld' unstable", key);
-		sym_list = slist_remove(sym_list, sym);
-		node_free(sym->node);
-	} else {
-		sym = xmalloc(sizeof(*sym));
-		sym->line_number = line_number;
 	}
-	sym->node = newn;
-	sym_list = slist_prepend(sym_list, sym);
-	dict_insert(table, (void *)key, sym_list);
+	if (old_lnode) {
+		gl_list_remove_node(sym_list, old_lnode);
+	}
+	gl_sortedlist_add(sym_list, sym_local_compar, new_sym);
 }
