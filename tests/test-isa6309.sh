65903ca058896a811163fc02948062857fed1a82
Ciaran Anscomb <www@6809.org.uk>
2015-08-31 09:11:23 +0100

    tests: use SREC output for comparisons
    Text files are nicer to distribute than binaries.

--- tests/test-isa6309.sh
+++ tests/test-isa6309.sh
@@ -4,7 +4,7 @@ fail=0
 tests="isa6309-immediate isa6309-direct isa6309-indexed isa6309-extended isa6309-inherent"
 
 for t in ${tests}; do
-	../src/asm6809${EXEEXT} -3 -B -l ${t}.lis -o ${t}.out ${t}.s
+	../src/asm6809${EXEEXT} -3 -S -l ${t}.lis -o ${t}.out ${t}.s
 	cmp ${t}.out ${t}.cmp || fail=1
 done
 
--- tests/test-isa6809.sh
+++ tests/test-isa6809.sh
@@ -4,7 +4,7 @@ fail=0
 tests="isa6809-direct isa6809-extended isa6809-immediate isa6809-indexed isa6809-inherent isa6809-relative"
 
 for t in ${tests}; do
-	../src/asm6809${EXEEXT} -B -l ${t}.lis -o ${t}.out ${t}.s
+	../src/asm6809${EXEEXT} -S -l ${t}.lis -o ${t}.out ${t}.s
 	cmp ${t}.out ${t}.cmp || fail=1
 done
 
