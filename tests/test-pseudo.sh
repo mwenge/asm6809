7ec8215a3887de77bb62bc90faf4f3240a618626
Ciaran Anscomb <www@6809.org.uk>
2016-05-20 06:44:59 +0100

    tests: add section test
    Would catch bug fixed by Stewart Orchard.

--- tests/test-pseudo.sh
+++ tests/test-pseudo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 fail=0
-tests="pseudo-cond pseudo-org-put-setdp"
+tests="pseudo-cond pseudo-org-put-setdp pseudo-section"
 
 for t in ${tests}; do
 	../src/asm6809${EXEEXT} -S -l ${t}.lis -o ${t}.out ${t}.s
