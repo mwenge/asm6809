d09391dcf85a8c3127099d25c704289efc9f4839
Ciaran Anscomb <www@6809.org.uk>
2016-06-02 20:28:36 +0100

    rejig doc directory, .gitignores

--- doc/make-pdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-groff -Tpdf -P-pa4 -t -mpdfmark -mandoc ../man/asm6809.1 > asm6809.pdf
--- doc/make-text.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-groff -Tascii -P-bcou -t -mpdfmark -mandoc ../man/asm6809.1 > asm6809.txt
