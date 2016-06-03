050039803cd6f0072b4bd008ac9263a7184d430f
Ciaran Anscomb <www@6809.org.uk>
2016-06-03 08:19:05 +0100

    update copyright dates

--- dt101/array.h
+++ dt101/array.h
@@ -1,7 +1,7 @@
 /*
 
 C array handling
-Copyright 2014-2015, Ciaran Anscomb
+Copyright 2014-2016, Ciaran Anscomb
 
 This is free software; you can redistribute it and/or modify it under
 the terms of the GNU Lesser General Public License as published by the
